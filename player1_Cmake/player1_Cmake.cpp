c8096bfeb3da8caf8fa47f2d055c48de75973425
Platon <i1080p-ti@live.ru>
2021-10-05 19:31:42 +0300

    Добавьте файлы проекта.

---
+++ player1_Cmake/player1_Cmake.cpp
@@ -0,0 +1,25 @@
+﻿// player1_Cmake.cpp: определяет точку входа для приложения.
+//
+
+#include "player1_Cmake.h"
+#include <clocale>
+
+using namespace std;
+
+int x = 3;
+
+int main()
+{
+	setlocale(0, "Russian");
+
+	cout << "Вводите значение x и нажимайте Enter. - " << endl;
+	cin >> x;
+	cout << "" << endl;
+	cout << "Hello CMake." << endl;
+
+
+	if (x < 4) {
+		cout << "Hello CMake 22 ." << endl;
+	}
+	return 0;
+}
